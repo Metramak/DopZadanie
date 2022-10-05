db8739b2aa3588daab1e46df8e0e96ec6e6c89cb
Maxim Timofeev <metramaksec@yandex.ru>
2022-10-05 16:45:05 +0300

    Create funcs.h, funcs.cpp, initialize development

---
+++ funcs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "funcs.h"
+
+double SumFun(double a, double b)
+{
+
+}
+
+double SubFun(double a, double b)
+{
+
+}
+
+double MultFun(double a, double b)
+{
+
+}
+
+double DivFun(double a, double b)
+{
+
+}
