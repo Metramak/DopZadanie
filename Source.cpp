e6cae42a060d98636907b1c7a9af5cbb0d9677a9
Metramak <boomox.ff@gmail.com>
2022-10-05 18:20:11 +0300

    Added changes to funcs

--- Source.cpp
+++ Source.cpp
@@ -29,10 +29,14 @@ int main()
 			cout << "\nResult: " << SumFun(a, b) << endl << endl;
 			break;
 		case '/':
-			cout << "\nFeature in development...\n\n";
+			cout << "\nEnter to digits with space: ";
+			cin >> a >> b;
+			cout << "\nResult: " << DivFun(a, b) << endl << endl;
 			break;
 		case '*':
-			cout << "\nFeature in development...\n\n";
+			cout << "\nEnter to digits with space: ";
+			cin >> a >> b;
+			cout << "\nResult: " << MultFun(a, b) << endl << endl;
 			break;
 		case 'e':
 			cout << "\nExiting...\n\n";
